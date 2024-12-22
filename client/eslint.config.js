bcaaf30d1deb33041a82f06db050862af44aa735
Viktor Kushnir <mustbefail@gmail.com>
2024-12-22 13:25:54 +0200

    chore: separate client and server

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,25 +3,29 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'vite.config.js'] },
+  prettierRecommended,
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+        sourceType: 'module'
+      }
     },
     settings: { react: { version: '18.3' } },
     plugins: {
       react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -29,10 +33,18 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+      'prettier/prettier': [
+        'error',
+        {
+          arrowParens: 'always',
+          printWidth: 120,
+          semi: false,
+          singleQuote: true,
+          trailingComma: 'none'
+        }
+      ]
+    }
+  }
 ]
---
+++ server/eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default [
+  { ignores: ['dist'] },
+  prettierRecommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.node,
+        ...globals.es2025
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+      'prettier/prettier': [
+        'error',
+        {
+          arrowParens: 'always',
+          printWidth: 120,
+          semi: false,
+          singleQuote: true,
+          trailingComma: 'none'
+        }
+      ]
+    }
+  }
+]
