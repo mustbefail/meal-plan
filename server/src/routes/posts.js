f3e172999a9f20bbc9e8a67eab4a96271de9c925
Viktor Kushnir <mustbefail@gmail.com>
2024-12-29 02:10:03 +0200

    feat: add post, pacth, delete to our posts routes
    Signed-off-by: Viktor Kushnir <mustbefail@gmail.com>

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,4 +1,12 @@
-import { getPostById, listAllPosts, listPostsByAuthor, listPostsByTag } from '../services/posts.js'
+import {
+  createPost,
+  deletePost,
+  getPostById,
+  listAllPosts,
+  listPostsByAuthor,
+  listPostsByTag,
+  updatePost
+} from '../services/posts.js'
 
 export function postsRoutes(app) {
   app.get('/api/v1/posts', async (req, res) => {
@@ -34,6 +42,38 @@ export function postsRoutes(app) {
     } catch (error) {
       console.error(`Error fetching post: ${error}`)
       return res.status(500).json({ error: 'internal server error' }).end()
+    }
+  })
+
+  app.post('/api/v1/posts', async (req, res) => {
+    try {
+      const post = await createPost(req.body)
+      return res.status(201).json(post)
+    } catch (error) {
+      console.error(`Error creating post: ${error}`)
+      return res.status(500).json({ error: 'error creating post' }).end()
+    }
+  })
+
+  app.patch('/api/v1/posts/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+      const post = await updatePost(id, req.body)
+      return res.json(post)
+    } catch (error) {
+      console.error(`Error creating post: ${error}`)
+      return res.status(500).json({ error: `error updating post` }).end()
+    }
+  })
+  app.delete('/api/v1/posts/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+      const { deletedCount } = await deletePost(id)
+      return deletedCount > 0 ? res.status(204).end() : res.status(404).end()
+    } catch (error) {
+      console.error(`Error deleting post: ${error}`)
+      return res.status(500).json({ error: `error deleting post` }).end()
     }
   })
 }
